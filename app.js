f9d19b40e4fc3b9567b0808bdc34d6dbc07dd6cd
sshrik <chj878194@naver.com>
2019-04-29 00:04:36 +0900

    Add image streaming routes

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 var express = require("express");
+//var bodyParser = require('body-parser');
 var app = express();
+
+var ims = require("./routes/imageStreamingRouter.js")(app);
 
 app.use(express.static("public"));
 app.use(express.static("views"));
 
+app.use('/ims',ims);
+
 app.set('view engine','ejs');
+app.engine('html', require('ejs').renderFile);
 app.set('views','./views');
+//app.use(bodyParser.json());
 
 app.listen(35008, function(){
   console.log("Noa의 서버가 열렸다.");
